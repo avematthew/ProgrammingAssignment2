2d0675bf9091eb847bf056eb469e5a3eda6521bf
MatthewTurnbull <ave.jor@gmail.com>
2015-05-05 21:08:33 -0500

    add comments
    Signed-off-by: MatthewTurnbull <ave.jor@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a script produced for an assignment for R programming, whose goal is to solve matrices while caching their solutions
+## makeCacheMatrix creates an object which can be used by the cacheSolve function.
+## cacheSolve checks to see if the passed matrix object already has a solution, and if not it calculates one. Otherwise it retrieves the cached solution.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as its argument and produces a cached matrix object for use by cacheSolve
+## it initializes the matrix solution in the cache as a null value
+## it contains functions to retrieve the matrix object used to create it, and to retrieve the solution to that object once it exists
 
 makeCacheMatrix <- function(x = matrix()) {
+	#initialize the matrix solution as a NULL value
 	solvedmatrix <- NULL
+	#set the matrix values
 	setmatrix <- function(input){
 		x <<- input
 		solvedmatrix <<- NULL
 	}
+	#cache the solution
 	setsolvedmatrix<-function(solution){
 		solvedmatrix <<- solution
 	}
+	#return the matrix values
 	getmatrix <- function(){
 		x
 	}
+	#return the solved matrix
 	getsolvedmatrix<-function(){
 		solvedmatrix
 	}
@@ -22,18 +30,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the solution for a cacheMatrix object created by makeCacheMatrix.
+## it first checks if a solution has already been created, and then creates one if not
+## if it does create a solution, it caches that solution
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## get the cached solution
 	solvedmatrix <- x$getsolvedmatrix()
+	## if a cached solution existed and was not the initial value of NULL, then return it
 	if(!is.null(solvedmatrix)){
 		message("retrieving cached matrix solution")
 		return(solvedmatrix)
 	}
+	## if no solution existed, load the matrix and solve it.
 	data <- x$getmatrix()
 	solvedmatrix <- solve(data, ...)
+	#cache the solution if you had to make one
 	x$setsolvedmatrix(solvedmatrix)
+	#return the solved matrix
 	solvedmatrix
 	
 }
