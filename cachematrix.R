28aaaf343c531d7f940c518d6305687aa330922e
MatthewTurnbull <ave.jor@gmail.com>
2015-05-05 20:49:09 -0500

    initial draft
    Signed-off-by: MatthewTurnbull <ave.jor@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	solvedmatrix <- NULL
+	setmatrix <- function(input){
+		x <<- input
+		solvedmatrix <<- NULL
+	}
+	setsolvedmatrix<-function(solution){
+		solvedmatrix <<- solution
+	}
+	getmatrix <- function(){
+		x
+	}
+	getsolvedmatrix<-function(){
+		solvedmatrix
+	}
 }
 
 
@@ -12,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	solvedmatrix <- x$getsolvedmatrix()
+	if(!is.null(solvedmatrix)){
+		message("retrieving cached matrix solution")
+		return(solvedmatrix)
+	}
+	data <- x$getmatrix()
+	solvedmatrix <- solve(data, ...)
+	x$setsolvedmatrix(solvedmatrix)
+	solvedmatrix
+	
 }
